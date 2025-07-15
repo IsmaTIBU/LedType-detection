a46e97cfea2a8952e4dc3ba6ff6d57db4f2e9a4d
IsmaTIBU <01ismaelmon@gmail.com>
2025-07-15 14:55:01 +0200

    final updt

--- detection.py
+++ detection.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 model = YOLO('best.pt')
 
 # Seleccion de camara predefinida (0)
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
